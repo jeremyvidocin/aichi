8a982f439d53013f9ef1fd253e1afd78066ea2e1
VIDOCIN--BORDIN Jérémy <jeremyvidocin@gmail.com>
2024-11-04 15:39:14 -0400

    Solution trouvée
    Connexion réussie manque du DRIVER.

--- src/modele/ConnexionDAO.java
+++ src/modele/ConnexionDAO.java
@@ -10,6 +10,14 @@ public class ConnexionDAO {
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
--- src/modele/UserDAO.java
+++ src/modele/UserDAO.java
@@ -8,16 +8,20 @@ import java.sql.SQLException;
 public class UserDAO {
 
     public User authenticate(String nom, String motDePasse) {
-        String query = "SELECT * FROM utilisateurs WHERE nom = ? AND MotDePasse = ?";
+        String query = "SELECT * FROM utilisateurs WHERE Nom = ? AND MotDePasse = ?";
         try (Connection connection = ConnexionDAO.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
 
             statement.setString(1, nom);
             statement.setString(2, motDePasse);
 
+            System.out.println("Query: " + query);
+            System.out.println("Nom: " + nom);
+            System.out.println("MotDePasse: " + motDePasse);
+
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
-                    int id = resultSet.getInt("Id");
+                    int id = resultSet.getInt("ID");
                     String typeAcces = resultSet.getString("TypeAcces");
                     return new User(id, nom, motDePasse, typeAcces);
                 }
